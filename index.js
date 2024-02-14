1115a44d39fe7204bd5cab0d74dacb7b37e6d540
Raphael Mello <raphaelpmros@hotmail.com>
2024-02-14 17:17:18 -0300

    deixando links funcionais para cada campground ter sua propria pagina

--- index.js
+++ index.js
@@ -26,6 +26,35 @@ app.get('/campgrounds', async (req,res) => {
     res.render('campgrounds/index', {campgrounds})
 })
 
+app.get("/campgrounds/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      con.query(
+        `SELECT * FROM campground WHERE id = '${id}'`,
+        function (err, result, fields) {
+          if (err) {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
+            return;
+          }
+          if (result.length === 0) {
+            res.status(404).send("Campground not found");
+            return;
+          }
+          const campground = result[0]; // Corrigido para 'campground' (singular)
+          console.log(campground);
+          res.render("campgrounds/show", { campground }); // Corrigido para 'campground' (singular)
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    }
+});
+
+
+
+
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to database");
