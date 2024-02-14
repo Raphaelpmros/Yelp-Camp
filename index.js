b35b5089dc5e321b109691d87805e2cbf502cb2d
Raphael Mello <raphaelpmros@hotmail.com>
2024-02-14 16:32:31 -0300

    criando a primeira estrutura da pagina com titulo dos campgrounds

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const con = require("./database/db");
+const { rejects } = require('assert');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const allCampgrounds = new Promise((resolve, reject) => {
+    con.query("SELECT * FROM campground", function (err, result) {
+        if(err){
+            reject(err);
+        } else {
+            resolve(result);;
+        }
+    });
+});
+
 app.get('/', (req,res) => {
     res.render('home');
+})
+
+app.get('/campgrounds', async (req,res) => {
+    const campgrounds = await allCampgrounds;
+    res.render('campgrounds/index', {campgrounds})
 })
 
 con.connect(function(err) {
