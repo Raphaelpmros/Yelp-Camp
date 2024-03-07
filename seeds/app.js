f0b3cd2894a9163767c31c7b8eaad2183d0f9bce
Raphael Mello <raphaelpmros@hotmail.com>
2024-03-07 17:54:54 -0300

    adicionando algumas medidas de segurança

--- seeds/app.js
+++ seeds/app.js
@@ -16,10 +16,19 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
   try {
     await new Promise((resolve, reject) => {
-      const sql = "DELETE FROM campground";
-      con.query(sql, (err, result) => {
+      const sqlDeleteReviews = "DELETE FROM reviews WHERE id_camp IN (SELECT id FROM campground)";
+      con.query(sqlDeleteReviews, (err, result) => {
         if (err) reject(err);
-        console.log("Number of records deleted: " + result.affectedRows);
+        console.log("Número de registros deletados em reviews: " + result.affectedRows);
+        resolve();
+      });
+    });
+
+    await new Promise((resolve, reject) => {
+      const sqlDeleteCampground = "DELETE FROM campground";
+      con.query(sqlDeleteCampground, (err, result) => {
+        if (err) reject(err);
+        console.log("Número de registros deletados em campground: " + result.affectedRows);
         resolve();
       });
     });
@@ -31,10 +40,10 @@ const seedDB = async () => {
       const title = `${sample(descriptors)} ${sample(places)}`;
       const price = priceRandom;
       const description = "dasndjnasidsidnasidnsajdna asjdnaijdnsjidnaijdnjaisnaidnsad nasjidasijdnijdnasjidnaidnifbehbefhuwbfew ibfirgpwfwep oeifjweiofje wfpowejfpwe ofiejfow fjeop";
-      const sql = `INSERT INTO campground (title, price, description, location, image, author) VALUES ('${title}', '${price}', '${description}', '${location}', 'https://source.unsplash.com/collection/483251', 2)`;
+      const sqlInsertCampground = `INSERT INTO campground (title, price, description, location, image, author) VALUES ('${title}', '${price}', '${description}', '${location}', '["https://res.cloudinary.com/dhafchcdg/image/upload/v1709755016/YelpCamp/xjbb3qquezd9erpzuah8.png","https://res.cloudinary.com/dhafchcdg/image/upload/v1709755017/YelpCamp/j6i5aie1a0fdkbf7kmgl.png","https://res.cloudinary.com/dhafchcdg/image/upload/v1709755018/YelpCamp/u0f5k7sbb8nn45drs63z.png","https://res.cloudinary.com/dhafchcdg/image/upload/v1709755023/YelpCamp/shqmfpfzbyk4ttriqcsw.png","https://res.cloudinary.com/dhafchcdg/image/upload/v1709756049/YelpCamp/u4jfvat9s87nnvfykski.png","https://res.cloudinary.com/dhafchcdg/image/upload/v1709756052/YelpCamp/pxx2rgsg6l8iwk9lbhbc.png","https://res.cloudinary.com/dhafchcdg/image/upload/v1709756796/YelpCamp/rh3dube5jvgpmqcj8ts3.png"]', 2)`;
 
       await new Promise((resolve, reject) => {
-        con.query(sql, (err, result) => {
+        con.query(sqlInsertCampground, (err, result) => {
           if (err) reject(err);
           console.log("Inserido");
           resolve();
